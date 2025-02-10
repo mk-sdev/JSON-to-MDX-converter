6710177faf2382d9ec8d8039b850ee002447d664
Mikołaj <kaleta222@gmail.com>
2025-02-10 10:34:57 +0100

    fix: Aside and Img imports

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function changeToMDX() {
       out += component
     })
 
-  if (imports.size>0) out = [...imports].join('\n') + '\n\n' + out
+  if (imports.size > 0) out = [...imports].join('\n') + '\n\n' + out
 
   console.log(out)
   output.textContent = out // Wyświetlamy w output
@@ -59,8 +59,10 @@ function processComponent(component, imports) {
       if (imports) imports.add('import YT from "../../../components/YT.astro"')
       return `<YT id={"${component.value}"} />\n\n`
     case 'Image':
-      if (imports)
+      if (imports) {
+        imports.add('import image from "../../../assets/image.png"')
         imports.add('import Img from "../../../components/Img.astro"')
+      }
       if (component.props)
         return `<Img src={"${component.value}"} description="${component.props.description}"/>\n\n`
       else return `<Img src={"${component.value}"} />\n\n`
@@ -68,7 +70,7 @@ function processComponent(component, imports) {
       return `\`\`\`${component.props.language}\n${component.value}\n\`\`\`\n\n` // Obsługa komponentu Code
     case 'Block':
       if (imports)
-        imports.add('import Aside from "../../../components/Aside.astro"')
+        imports.add('import { Aside } from "@astrojs/starlight/components"')
       let type = component.props.type
       if (type === 'info') type = 'note'
       else if (type === 'warning') type = 'caution'
